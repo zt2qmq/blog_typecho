cb06ce6632752ef9abae789b45ff3e5e28a06c59
zt2qmq <854972252@qq.com>
2020-04-05 20:58:11 +0800

    delext some theme

--- blog.zt2qmq.top/config.inc.php
+++ blog.zt2qmq.top/config.inc.php
@@ -63,7 +63,7 @@ $db->addServer(array (
   'host' => 'localhost',
   'user' => 'root',
   'password' => '123456',
-  'charset' => 'utf8',
+  'charset' => 'utf8mb4',
   'port' => '3306',
   'database' => 'typecho',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
